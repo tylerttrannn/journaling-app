815225ed163237d8ca40ed297f1c77141eb21f3b
tylerttrannn <tylerttrann@gmail.com>
2024-09-23 17:55:50 -0700

    streamlined css colors styling by using variables instead

--- frontend_client/src/main.jsx
+++ frontend_client/src/main.jsx
@@ -15,7 +15,6 @@ import Dashboard from './views/Dashboard/Dashboard.jsx'
 import Settings from './views/Settings/Settings.jsx'
 import ForgotPassword from './views/ForgotPassword/ForgotPassword.jsx'
 
-import MyEditor from './components/Header/MyEditor.jsx';
 
 
 const router = createBrowserRouter([
@@ -72,11 +71,7 @@ const router = createBrowserRouter([
 
  },
 
- {
-  path: '/MyEditor',
-  element: <MyEditor/>
 
- }
 
 
 ]);
